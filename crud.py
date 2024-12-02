f623eeba293d3da1da6afe1456ba073d04ad4d8f
3agoskin <kirillzagoskin.work@gmail.com>
2024-12-02 05:12:16 +0300

    new `create_posts` func & new posts in db

--- crud.py
+++ crud.py
@@ -60,9 +60,37 @@ async def show_users_with_profiles(session: AsyncSession):  # -> list[User]:
         )
 
 
+async def create_posts(
+    session: AsyncSession,
+    user_id: int,
+    *posts_titles: str,
+) -> list[Post]:
+    posts = [Post(title=title, user_id=user_id) for title in posts_titles]
+    session.add_all(posts)
+    await session.commit()
+    return posts
+
+
 async def main():
     async with db_helper.session_factory() as session:
-        await show_users_with_profiles(session=session)
+        user_yuri = await get_user_by_username(session=session, username="yuri")
+        user_john = await get_user_by_username(session=session, username="john")
+        if user_yuri:
+            await create_posts(
+                session,
+                user_yuri.id,
+                "New transfer to Spartak",
+                "Questions for Mutko",
+                "The game for jopa",
+            )
+        if user_john:
+            await create_posts(
+                session,
+                user_john.id,
+                "How to defeat Skynet!",
+                "Don't give up",
+                "Types of terminators",
+            )
 
 
 if __name__ == "__main__":
