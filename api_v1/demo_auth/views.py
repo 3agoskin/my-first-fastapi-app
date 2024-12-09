b31fb4bb850a4bd4413d60ee5f022b3d13907944
3agoskin <kirillzagoskin.work@gmail.com>
2024-12-10 02:22:17 +0300

    auth by some http header

--- api_v1/demo_auth/views.py
+++ api_v1/demo_auth/views.py
@@ -1,7 +1,7 @@
 import secrets
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status, Header
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
 router = APIRouter(prefix="/demo-auth", tags=["Demo Auth"])
@@ -58,8 +58,28 @@ def demo_basic_auth_username(auth_username: str = Depends(get_auth_user_username
     }
 
 
+static_auth_token_to_username = {
+    "54cde5389311f2112e0c976bf86533a2": "admin",
+    "288914f057891b60d4af3b9cf0d3af87": "john",
+}
+
+
+def get_auth_username_by_static_auth_token(
+    static_token: str = Header(alias="x-auth-token"),
+) -> str:
+    if static_token not in static_auth_token_to_username:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token invalid",
+        )
+
+    return static_auth_token_to_username[static_token]
+
+
 @router.get(path="/some-http-header-auth/")
-def demo_auth_some_http_header(username: str = Depends(...)):
+def demo_auth_some_http_header(
+    username: str = Depends(get_auth_username_by_static_auth_token),
+):
     return {
         "message": f"Hello, {username}!",
         "username": username,
